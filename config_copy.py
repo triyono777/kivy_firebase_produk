d3486cca7d822eb298c191ba3f1bc02f71cbe24a
triyono <yono.tri@gmail.com>
2024-10-25 10:13:26 +0700

    update gitignore

---
+++ config_copy.py
@@ -0,0 +1,12 @@
+# config.py
+def get_firebase_config():
+    config = {
+        "apiKey": "----",
+        "authDomain": "---",
+        "databaseURL": "-",
+        "projectId": "-",
+        "storageBucket": "-",
+        "messagingSenderId": "-",
+        "appId": "-"
+    }
+    return config
